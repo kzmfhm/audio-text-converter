244635c687e07e62bf5ff179a8891157a3c5c31d
kzmfhm <khuzaimanazik4@gmail.com>
2023-09-10 17:48:10 +0500

    Updated background

--- script.js
+++ script.js
@@ -1,50 +1,49 @@
-var SpeechRecognition = window.webkitSpeechRecognition;
+$(document).ready(function () {
+  var SpeechRecognition = window.webkitSpeechRecognition;
+  var recognition = new SpeechRecognition();
+  var Textbox = $("#textarea");
+  var instructions = $("#instructions");
 
-var recognition = new SpeechRecognition();
-var Textbox = $("#textarea");
-var instructions = $("#instructions");
+  var Content = "";
+  var currentLang = 'en-US'; // Default language is English
 
-var Content = "";
-var currentLang = 'en-US'; // Default language is English
+  recognition.continuous = true;
 
-recognition.continuous = true;
-
-recognition.onresult = function (event) {
-  var current = event.resultIndex;
-  var transcript = event.results[current][0].transcript;
+  recognition.onresult = function (event) {
+    var current = event.resultIndex;
+    var transcript = event.results[current][0].transcript;
   
-  Content += transcript;
-  Textbox.val(Content);
-};
+    Content += transcript;
+    Textbox.val(Content);
+  };
 
-$("#start").on("click", function (e) {
-  if ($(this).text() == "Click here to Stop Recording") {
-    $(this).html("Click here to Start Recording");
-    $("#instructions").html("");
-    recognition.stop();
-  } else {
-    $(this).html("Click here to Stop Recording");
-    $("#instructions").html("Try Speaking, Voice Recognition is On, Contents will be displayed below");
-    if (Content.length) {
+  $("#start").on("click", function (e) {
+    if ($(this).text() == "Click here to Stop Recording") {
+      $(this).html("Click here to Start Recording");
+      $("#instructions").html("");
+      recognition.stop();
+    } else {
+      $(this).html("Click here to Stop Recording");
+      $("#instructions").html("<p style='color: #05100a; font-size: 20px;'>Try Speaking, Voice Recognition is On, Contents will be displayed below</p>");
       Content += " ";
+      recognition.lang = currentLang;
+      recognition.start();
+    }
+  });
+
+  Textbox.on("input", function () {
+    Content = $(this).val();
+  });
+
+  // Add language switching functionality
+  $("#switchLanguage").on("click", function () {
+    if (currentLang === 'en-US') {
+      currentLang = 'ur-PK'; // Switch to Urdu (Pakistan)
+      $("#switchLanguage").html("Switch to English");
+    } else {
+      currentLang = 'en-US'; // Switch back to English
+      $("#switchLanguage").html("Switch to Urdu");
     }
     recognition.lang = currentLang;
-    recognition.start();
-  }
-});
-
-Textbox.on("input", function () {
-  Content = $(this).val();
-});
-
-// Add language switching functionality
-$("#switchLanguage").on("click", function () {
-  if (currentLang === 'en-US') {
-    currentLang = 'ur-PK'; // Switch to Urdu (Pakistan)
-    $("#switchLanguage").html("Switch to English");
-  } else {
-    currentLang = 'en-US'; // Switch back to English
-    $("#switchLanguage").html("Switch to Urdu");
-  }
-  recognition.lang = currentLang;
+  });
 });
